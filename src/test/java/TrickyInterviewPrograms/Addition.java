d61f3b047fba72e9ad9936a80d242fc8ad9f1b3b
Rekha071422 <lenovo@DESKTOP-41LM133>
2022-03-04 15:54:13 +0530

    added

--- src/test/java/TrickyInterviewPrograms/Addition.java
+++ src/test/java/TrickyInterviewPrograms/Addition.java
@@ -5,7 +5,12 @@ public class Addition {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		///For git hub purp  ose
+		
+	}
+	
+	public void add (String str)
+	{
+		
 	}
 
 }
